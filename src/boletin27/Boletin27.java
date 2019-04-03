03869549c2c10a2616e606f221457792b204120f
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-04-03 11:57:31 +0200

    Boletin27

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,15 @@
+package boletin27;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class Boletin27 {
+
+    public static void main(String[] args) {
+        Calculadora obxCalc=new Calculadora();
+        obxCalc.setVisible(true);
+
+    }
+    
+}
